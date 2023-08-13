9dda561c067c6b6332cc3c00474e0cf3fa0a2a9d
Rustam Ismagilov <rustam.ismagilove@gmail.com>
2023-08-13 20:46:32 +0200

    Multiple changes

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,20 +2,36 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public float moveSpeed = 2.0f;
-    public float minXPosition = -10f; // The X coordinate where coins should respawn
+    public float xMoveSpeed = 2.0f; // Move speed along the X-axis
+    public float yMoveSpeed = 1.0f; // Move speed along the Y-axis
+    public float minYPosition = -4.35f;
+    public float maxYPosition = 4.35f;
+    public float minXPosition = -10f;
+
+    private bool movingUp = true;
+
     public int coinValue = 1; // The value of the coin when collected
 
     private void Update()
     {
         // Move the coin from right to left along the X-axis
-        transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        transform.Translate(Vector3.left * xMoveSpeed * Time.deltaTime);
 
-        
+        // Calculate the new vertical position using yMoveSpeed
+        float newYPosition = transform.position.y + (movingUp ? 1 : -1) * yMoveSpeed * Time.deltaTime;
+
+        // Ensure the new position is within bounds
+        newYPosition = Mathf.Clamp(newYPosition, minYPosition, maxYPosition);
+
+        // Update the obstacle's position
+        transform.position = new Vector3(transform.position.x, newYPosition, transform.position.z);
+
+        // Change movement direction if reached bounds
+        if (newYPosition >= maxYPosition || newYPosition <= minYPosition)
+        {
+            movingUp = !movingUp;
+        }
     }
-
-
-    
 
     private void OnTriggerEnter2D(Collider2D other)
     {
