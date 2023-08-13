073118c13b730f51ee8bc96f52e83f49c6c28e64
Rustam Ismagilov <rustam.ismagilove@gmail.com>
2023-08-13 23:37:18 +0200

    Added pause menu + control hints + Webgl build

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +7,12 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 3f;
     private Rigidbody2D rb;
 
+    // Flag that indicates whether the game is currently paused
+    private bool paused;
+
+    // Reference to the pause screen object in the scene
+    public GameObject pauseScreen;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -13,6 +20,13 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        //Check if the user has pressed the Escape key
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //then call CheckForPause method
+            CheckForPause();
+        }
+
         // Check for jump input
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
@@ -31,6 +45,25 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Obstacle"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    // Check for pause input
+    void CheckForPause()
+    {
+        if (!paused)
+        {
+            // if game paused then show the "paused" screen and freeze the game with Time.timeScale = 0;
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            // if game unpaused then hide the "paused" screen and unfreeze the game with Time.timeScale = 1;
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
         }
     }
 }
